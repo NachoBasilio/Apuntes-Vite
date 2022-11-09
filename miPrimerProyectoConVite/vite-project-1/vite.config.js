97fcd8068a20355002f8c02612f895653fdb04a2
IgnaDev <arcortoon@gmail.com>
2022-11-09 14:27:14 -0300

    Vamos a agregar un nuevo input

--- miPrimerProyectoConVite/vite-project-1/vite.config.js
+++ miPrimerProyectoConVite/vite-project-1/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(()=>{
+export default defineConfig(({command, mode})=>{
     const port = 3000
+
+    console.log(command, mode)
+    const env = loadEnv(mode, process.cwd())
+    console.log(env)
+    if (mode === "development"){
+        console.log('modo desarrollo')
+    }else{
+        console.log('modo produccion')
+    }
 
     return{
         server: {
